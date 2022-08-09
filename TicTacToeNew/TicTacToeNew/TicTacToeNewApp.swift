ed1910d2739049001a8f09e60832803715d87110
DharmiPatel93 <103190066+DharmiPatel93@users.noreply.github.com>
2022-08-09 16:40:40 +0530

    Add files via upload

---
+++ TicTacToeNew/TicTacToeNew/TicTacToeNewApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeNewApp.swift
+//  TicTacToeNew
+//
+//  Created by Dharmi Patel on 06/04/22.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeNewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
